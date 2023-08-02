3729579706b08aaebd5162bef285d95b2a7bda9a
Lasindu Anjana <lasindua@gmail.com>
2023-08-02 16:30:05 +0530

    customer add get

--- backend/controllers/customer.controller.js
+++ backend/controllers/customer.controller.js
@@ -4,39 +4,25 @@ const {
   uploadBytesResumable,
   getDownloadURL,
 } = require("firebase/storage");
-const { firebaseApp, storage } = require("../config/firebase.config");
+const { storage } = require("../config/firebase.config");
 
-const uploadFileToFirebaseStorage = async (customerID, file) => {
-  // try {
-  //   const storageRef = firebaseApp
-  //     .storage()
-  //     .ref()
-  //     .child(`documents/${customerID}/${file.originalname}`);
-  //   const snapshot = await storageRef.put(file);
-  // } catch (error) {
-  //   console.error("Error uploading file:", error);
-  //   throw error;
-  // }
-
-  const fileRef = ref(storage, `documents/${customerID}/${file.originalname}`);
+const uploadFileToFirebaseStorage = async (fileType, customerID, file) => {
+  const fileRef = ref(storage, `documents/${customerID}/${fileType + "-" + file.originalname}`);
 
   const metadata = {
     contentType: file.mimetype,
   };
 
-  uploadBytesResumable(fileRef, file.buffer, metadata)
-    .then((snapshot) => {
-      console.log("Uploaded a blob or file!");
-      getDownloadURL(fileRef)
-        .then((url) => {
-          console.log(url);
-          return url;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    })
-    .catch((err) => console.log(err));
+  try {
+    const snapshot = await uploadBytesResumable(fileRef, file.buffer, metadata);
+    console.log("Uploaded a blob or file!");
+    const url = await getDownloadURL(fileRef);
+    return url;
+
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
 };
 
 exports.addCustomer = async (req, res) => {
@@ -68,137 +54,59 @@ exports.addCustomer = async (req, res) => {
     const guarantorNICFrontCopy = req.files["guarantorNICFrontCopy"][0];
     const guarantorNICRearCopy = req.files["guarantorNICRearCopy"][0];
 
-    var NICFrontCopyLink;
-    var NICRearCopyLink;
-    var customerPhotoLink;
-    var guarantorNICFrontCopyLink;
-    var guarantorNICRearCopyLink;
+    const [
+      NICFrontCopyLink,
+      NICRearCopyLink,
+      customerPhotoLink,
+      guarantorNICFrontCopyLink,
+      guarantorNICRearCopyLink,
+    ] = await Promise.all([
+      uploadFileToFirebaseStorage("NICFrontCopy", customerID, NICFrontCopy),
+      uploadFileToFirebaseStorage("NICRearCopy", customerID, NICRearCopy),
+      uploadFileToFirebaseStorage("customerPhoto", customerID, customerPhoto),
+      uploadFileToFirebaseStorage("guarantorNICFrontCopy", customerID, guarantorNICFrontCopy),
+      uploadFileToFirebaseStorage("guarantorNICRearCopy", customerID, guarantorNICRearCopy),
+    ]);
 
-    // const applicationLink = await uploadFileToFirebaseStorage(
-    //   customerID,
-    //   application
-    // );
-    // const document1Link = await uploadFileToFirebaseStorage(
-    //   customerID,
-    //   document1
-    // );
-    // const document2Link = await uploadFileToFirebaseStorage(
-    //   customerID,
-    //   document2
-    // );
-
-    const applicationRef = ref(
-      storage,
-      `documents/${customerID}/${application.originalname}`
-    );
-
-    const applicationMetadata = {
-      contentType: application.mimetype,
-    };
-
-    await uploadBytesResumable(
-      applicationRef,
-      application.buffer,
-      applicationMetadata
-    )
-      .then((snapshot) => {
-        console.log("Uploaded a blob or file!");
-        getDownloadURL(applicationRef)
-          .then(async (url) => {
-            console.log(url);
-            applicationLink = url;
-
-            const document1Ref = ref(
-              storage,
-              `documents/${customerID}/${document1.originalname}`
-            );
-
-            const document1Metadata = {
-              contentType: document1.mimetype,
-            };
-
-            await uploadBytesResumable(
-              document1Ref,
-              document1.buffer,
-              document1Metadata
-            )
-              .then((snapshot) => {
-                console.log("Uploaded a blob or file!");
-                getDownloadURL(document1Ref)
-                  .then(async (url) => {
-                    console.log(url);
-                    document1Link = url;
-                    const document2Ref = ref(
-                      storage,
-                      `documents/${customerID}/${document2.originalname}`
-                    );
-
-                    const document2Metadata = {
-                      contentType: document2.mimetype,
-                    };
-
-                    await uploadBytesResumable(
-                      document2Ref,
-                      document2.buffer,
-                      document2Metadata
-                    )
-                      .then((snapshot) => {
-                        console.log("Uploaded a blob or file!");
-                        getDownloadURL(document2Ref)
-                          .then((url) => {
-                            console.log(url);
-                            document2Link = url;
-
-                            Customer.create({
-                              customerID,
-                              name,
-                              NIC,
-                              email,
-                              phone,
-                              address,
-                              description,
-                              guarantor,
-                              guarantorNIC,
-                              guarantorMobile,
-                              loanAmount,
-                              paidAmount: 0,
-                              noOfInstallments,
-                              startDate: new Date(
-                                startDate + "T00:00:00+05:30"
-                              ),
-                              billingCycle,
-                              nextBillingDate: new Date(
-                                startDate + "T00:00:00+05:30"
-                              ),
-                              applicationDocumentLink: applicationLink,
-                              document1Link: document1Link,
-                              document2Link: document2Link,
-                            })
-                              .then((result) => {
-                                // const customers = Customer.findMany({});
-                                res.status(200).json({ result: result });
-                              })
-                              .catch((err) => {
-                                res.status(400).json({ message: err.message });
-                              });
-                          })
-                          .catch((err) => {
-                            console.log(err);
-                          });
-                      })
-                      .catch((err) => console.log(err));
-                  })
-                  .catch((err) => {
-                    console.log(err);
-                  });
-              })
-              .catch((err) => console.log(err));
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+    Customer.create({
+      customerID,
+      name,
+      NIC,
+      email,
+      phone,
+      phoneTwo,
+      address,
+      description,
+      guarantor,
+      guarantorNIC,
+      guarantorMobile,
+      guarantorMobileTwo,
+      loanAmount,
+      installmentAmount,
+      paidAmount: 0,
+      noOfInstallments,
+      startDate: new Date(
+        startDate + "T00:00:00+05:30"
+      ),
+      billingCycle,
+      collectorId,
+      nextBillingDate: new Date(
+        startDate + "T00:00:00+05:30"
+      ),
+      NICFrontCopyLink,
+      NICRearCopyLink,
+      customerPhotoLink,
+      guarantorNICFrontCopyLink,
+      guarantorNICRearCopyLink
+    })
+      .then((result) => {
+        // const customers = Customer.findMany({});
+        res.status(200).json({ result: result });
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        res.status(400).json({ message: err.message });
+      });
+
   } catch (e) {
     console.log("running");
     res.status(400).json({ message: e.message });
@@ -208,21 +116,7 @@ exports.addCustomer = async (req, res) => {
 exports.getCustomers = async (req, res) => {
   Customer.find()
     .then((customers) => {
-      let customerDetails = [];
-
-      for (let customer of customers) {
-        let customerDetail = {};
-        customerDetail.name = customer.name;
-        customerDetail.loanAmount = customer.loanAmount;
-        customerDetail.paidAmount = customer.paidAmount;
-        customerDetail.noOfInstallments = customer.noOfInstallments;
-        customerDetail.startDate = customer.startDate;
-        customerDetail.customerID = customer.customerID;
-
-        customerDetails.push(customerDetail);
-      }
-
-      res.status(200).json({ customers: customerDetails });
+      res.status(200).json({ customers: customers });
     })
     .catch((err) => {
       console.log(err);

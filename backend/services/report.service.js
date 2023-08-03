ae3f723d4d14d4ac65795ae2af5e5e2c4dddcb68
Supul Edirisinghe <supul@surge.global>
2023-08-04 02:04:26 +0530

    refactoring functions

--- backend/services/report.service.js
+++ backend/services/report.service.js
@@ -1,52 +1,111 @@
 const fs = require('fs');
 const nodemailer = require('nodemailer');
-const events = require('events');
 const moment = require('moment')
 
-const date_time = moment().format("MMM Do YY");  
-const fileName = date_time + ' - Auto Credit Daily Report' +'.xlsx';
 
-//setup email
+// TODO: remove hardcoded values on stag
+const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS || "morning123lucifer@gmail.com";
+const EMAIL_PROVIDER = process.env.EMAIL_PROVIDER || "gmail"
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD || "ypgdraaoruolsgek"
+const COMPANY_NAME = "Auto Credit";
 
-var check =1;
-var events = new events.EventEmitter();
-var smtpTransport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: "morning123lucifer@gmail.com",
-        pass: "ypgdraaoruolsgek"
-    }
-});
+function getMailOptions(payload) {
+  const from = COMPANY_NAME + '<' + EMAIL_ADDRESS + '>';
+  const to = payload.to;
+  const subject = payload.date + ' -' + COMPANY_NAME + payload.reportType;
+  const text = 'File Attached for : ' + payload.date;
+  const html = '';
 
-function inputmail(){
-    ///////Email
-    const from = 'Auto Credit<morning123lucifer@gmail.com>';
-    const to  = 'supuledirisinghe@gmail.com';
-    const subject  = date_time + ' - Auto Credit Daily Report';
-    const text = 'File Attached for :' + date_time;
-    const html = '';
-    var mailOption = {
-        from: from,
-        to:  to,
-        subject: subject,
-        text: text,
-        html: html,
-        attachments: [{
-            filename: fileName,
-            path: fileName
-        }]
-    }
-    return mailOption;
+  var mailOption = {
+    from: from,
+    to: to,
+    subject: subject,
+    text: text,
+    html: html,
+    attachments: [{
+      filename: payload.fileName,
+      path: payload.filePath
+    }]
+  }
+  return mailOption;
 }
-function send(){
-        smtpTransport.sendMail(inputmail(),function(err,success){
-        if(err){
-            events.emit('error', err);
-        }
-        if(success){
-            events.emit('success', success);
-        }
-    });
+
+function getDate() {
+  return moment().format("MMM Do YY");
+}
+
+function generateDailyReport(records) {
+  const data = 'DAILY REPORT' + '\n' +
+    'Auto Credit' + '\n' +
+    getDate() + '\n' + '\n';
+
+  // extract the list of collectors
+  let collectors = new Set();
+  records.forEach((record) => {
+    collectors.add(record.collector)
+
+  })
+
+  let total = {};
+  for (const item of collectors) {
+    total[item] = 0;
+  }
+
+
+  // console.log(total);
+
+  let total_A = 0;
+  let total_B = 0;
+  let total_C = 0;
+
+  for (var i = 0; i < records.length; i++) {
+
+    if (records[i].collector == 'A') {
+      total_A += Number(records[i].amount);
+    } else if (records[i].collector == 'B') {
+      total_B += Number(records[i].amount);
+    } else {
+      total_C += Number(records[i].amount);
+    }
+
+    data = data + records[i].id + '\t' + records[i].name + '\t' + records[i].collector + '\t' + records[i].amount + '\n';
+  }
+
+  data = data + '\n' + '\n' + '\n';
+  data = data + "Collected Amount by A" + '\t' + total_A + '\n';
+  data = data + "Collected Amount by B" + '\t' + total_B + '\n';
+  data = data + "Collected Amount by C" + '\t' + total_C + '\n';
+
+
+  fs.writeFile((getDate() + ' - Auto Credit Daily Report' + '.xlsx'), data, (err) => {
+    if (err) {
+      throw err
+    };
+
+    console.log('File created');
+  });
+}
+
+function sendMail(payload) {
+  let transporter = nodemailer.createTransport({
+    service: EMAIL_PROVIDER,
+    auth: {
+      user: EMAIL_ADDRESS,
+      pass: EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = getMailOptions(payload);
+
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+
 }
 
 
@@ -71,67 +130,32 @@ function send(){
 
 
 
-var jsn = [{
-    "name": "Nilesh",
-    "id": "RDTC",
-    "collector":"A",
-    "amount": "1000"
-   },{
-    "name": "Nilesh",
-    "id": "RDTC",
-    "collector":"B",
-    "amount": "2000"
-   },{
-    "name": "Nilesh",
-    "id": "RDTC",
-    "collector":"B",
-    "amount": "3000"
- }];
+const records = [{
+  "name": "Nilesh",
+  "id": "RDTC",
+  "collector": "A",
+  "amount": "1000"
+}, {
+  "name": "Nilesh",
+  "id": "RDTC",
+  "collector": "B",
+  "amount": "2000"
+}, {
+  "name": "Nilesh",
+  "id": "RDTC",
+  "collector": "B",
+  "amount": "3000"
+}];
 
-var data='DAILY REPORT' + '\n' +
-         'Auto Credit' + '\n' +
-         date_time+ '\n' + '\n' 
-;
+const fileName = getDate() + ' - Auto Credit Daily Report' + '.xlsx'
+const payload = {
+  to: "supuledirisinghe@gmail.com",
+  fileName: fileName,
+  filePath: fileName,
+  reportType: "Daily Report",
+  date: getDate()
+};
 
-let total_A = 0;
-let total_B = 0;
-let total_C = 0;
+generateDailyReport(records);
 
-for (var i = 0; i < jsn.length; i++) {
-    
-    if(jsn[i].collector == 'A'){
-        total_A += Number(jsn[i].amount);
-    }else if(jsn[i].collector == 'B'){
-        total_B += Number(jsn[i].amount);
-    }else{
-        total_C += Number(jsn[i].amount);
-    }
-
-    data=data+jsn[i].id+'\t'+jsn[i].name+'\t'+jsn[i].collector+'\t'+jsn[i].amount+'\n';
- }
-
-data = data + '\n' +'\n' + '\n' ;
-data = data + "Collected Amount by A" +'\t'+ total_A + '\n';
-data = data + "Collected Amount by B" +'\t'+ total_B + '\n';
-data = data + "Collected Amount by C" +'\t'+ total_C + '\n';
-
-
-fs.writeFile( (date_time + ' - Auto Credit Daily Report' +'.xlsx'), data, (err) => {
-    if (err) throw err;
-    console.log('File created');
-
-    //send email
-    send();
-    events.on("error", function(err){
-        console.log("Mail not send");
-        if(check<10)
-        send();
-        check++;
-    });
-    events.on("success", function(success){
-        console.log("Mail send");
-    });
- 
-});
-
-
+sendMail(payload)

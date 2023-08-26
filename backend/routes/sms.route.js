5767dba0c0f437b61bb036e8065899f40074e64a
Lasindu Anjana <lasindua@gmail.com>
2023-08-26 16:17:56 +0530

    sms gateway info to backend

--- backend/routes/sms.route.js
+++ backend/routes/sms.route.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const passport = require("passport");
 const { checkPermission } = require("../middleware/userAuth");
 
-const { sendSMS } = require("../controllers/sms.controller")
+const { sendSMS, smsGatewayStatus } = require("../controllers/sms.controller");
 
-router.post( "/sendSMS", sendSMS )
-
+router.post("/sendSMS", sendSMS);
+router.get(
+  "/status",
+  [
+    passport.authenticate("jwt", { session: false }),
+    checkPermission(["admin"]),
+  ],
+  smsGatewayStatus
+);
 
 module.exports = router;

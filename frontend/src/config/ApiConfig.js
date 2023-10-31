81c0a2b122a449dd02abfb486f073a0d4e857951
sanjueranga <sanjueranga16@gmail.com>
2023-11-01 01:12:58 +0530

    what

--- frontend/src/config/ApiConfig.js
+++ frontend/src/config/ApiConfig.js
@@ -1,3 +1,4 @@
-const BASE_URL = "https://autocredit.semicolondevs.com/api/v1/";
+// const BASE_URL = "https://autocredit.semicolondevs.com/api/v1/";
+const BASE_URL ="http://localhost:8080/api/v1"
 
 export default BASE_URL;

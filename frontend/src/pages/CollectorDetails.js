feda11afced070aff16637082cbb04b3f249efaa
sanjueranga <sanjueranga16@gmail.com>
2023-11-06 14:51:41 +0530

    collector data mapped to front

--- frontend/src/pages/CollectorDetails.js
+++ frontend/src/pages/CollectorDetails.js
@@ -31,10 +31,10 @@ const CollectorDetails = () => {
     var modifiedDate = new Date(date).toLocaleDateString("en-CA", {
       timeZone: "Asia/Colombo",
     });
-
+    console.log(modifiedDate + "here");
     const axiosConfig = {
       method: "GET",
-      url: `${BASE_URL}collectors/collectors/${modifiedDate}`,
+      url: `${BASE_URL}collector/collectors/${modifiedDate}`,
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -56,7 +56,6 @@ const CollectorDetails = () => {
     getCollectors();
   }, [date]);
 
-
   return (
     <>
       <SectionTitle title="Collector Details" />
@@ -66,13 +65,12 @@ const CollectorDetails = () => {
           <MUIDatePicker setDate={setDate} />
         </div>
       </div>{" "}
-
       <div className="bg-white drop-shadow-lg rounded-lg p-3 mb-5">
         <SectionSubtitle title="Daily transactions" />
         <div className="hidden lg:grid lg:grid-cols-7 lg:border-y lg:border-grey lg:py-3 lg:my-3">
           <p className="font-semibold col-span-2">Collector name</p>
           <p className="font-semibold">Collector ID</p>
-          <p className="font-semibold">Amount to be collected</p>
+          <p className="font-semibold">Collected Amount</p>
           <p className="font-semibold text-end">Workig Day</p>
         </div>
         <div className="overflow-y-auto max-h-96">
@@ -96,27 +94,26 @@ const CollectorDetails = () => {
               return (
                 <div
                   className="bg-yellow p-3 rounded-lg mb-3 lg:bg-transparent lg:p-0 lg:rounded-none lg:mb-0 w-full grid grid-cols-2 lg:grid-cols-7"
-                  key={collector._id}
+                  key={collector.id}
                 >
                   <p className="flex gap-1 lg:col-span-2 capitalize font-semibold lg:font-normal">
                     {collector.name}
                   </p>
                   <p className="flex gap-1 font-semibold lg:font-normal">
                     <span className=" flex lg:hidden"> -</span>
-                    {collector.email}
+                    {collector.id}
                   </p>
                   <p className="flex gap-1 col-span-2 lg:col-span-1">
                     <span className="flex lg:hidden">Collected by :</span>
                     <span className="capitalize">
-                      {collector.isTodayWorkingDay}
+                      {collector.totalCollected &&
+                        CurrencyFormatter(collector.totalCollected)}{" "}
+                      LKR
                     </span>
                   </p>
-                 
+
                   <p className="gap-1 flex lg:justify-end col-span-2 lg:col-span-1">
                     <span className="flex lg:hidden">Amount :</span>
-                    {collector.totalCollected &&
-                      CurrencyFormatter(collector.totalCollected)}{" "}
-                    LKR
                   </p>
                 </div>
               );

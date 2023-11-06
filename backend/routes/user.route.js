012faeb6eed56e7318220e6691e836206b3d4d9c
sanjueranga <sanjueranga16@gmail.com>
2023-11-06 14:09:37 +0530

    getCustomer details by date

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,67 +3,76 @@ const router = express.Router();
 const passport = require("passport");
 const { checkPermission } = require("../middleware/userAuth");
 const {
-    deleteUser,
-    updateProfile,
-    getPendingUsers,
-    getCollectors,
-    markWorkingDay
+  deleteUser,
+  updateProfile,
+  getPendingUsers,
+  getCollectors,
+  markWorkingDay,
+  getCollectorsByDate,
 } = require("../controllers/user.controller");
 
 router.delete(
-    "/delete/:id",
-    [
-        passport.authenticate("jwt", {
-            session: false
-        }),
-        checkPermission(["admin"]),
-    ],
-    deleteUser
+  "/delete/:id",
+  [
+    passport.authenticate("jwt", {
+      session: false,
+    }),
+    checkPermission(["admin"]),
+  ],
+  deleteUser
 );
 
 router.post(
-    "/update-profile/:id",
-    [
-        passport.authenticate("jwt", {
-            session: false
-        }),
-        checkPermission(["admin", "collector"]),
-    ],
+  "/update-profile/:id",
+  [
+    passport.authenticate("jwt", {
+      session: false,
+    }),
+    checkPermission(["admin", "collector"]),
+  ],
 
-    updateProfile
-)
+  updateProfile
+);
 
 router.post("/mark-working/:id", [
   passport.authenticate("jwt", {
     session: false,
   }),
   checkPermission(["admin", "collector"]),
-  markWorkingDay
+  markWorkingDay,
 ]);
 
-
 router.get(
-    "/pending-users",
-    [
-        passport.authenticate("jwt", {
-            session: false
-        }),
-        checkPermission(["admin"]),
-    ],
-    getPendingUsers
+  "/pending-users",
+  [
+    passport.authenticate("jwt", {
+      session: false,
+    }),
+    checkPermission(["admin"]),
+  ],
+  getPendingUsers
 );
 
 router.get(
-    "/collectors",
-    [
-        passport.authenticate("jwt", {
-            session: false
-        }),
-        checkPermission(["admin"]),
-    ],
-    getCollectors
+  "/collectors",
+  [
+    passport.authenticate("jwt", {
+      session: false,
+    }),
+    checkPermission(["admin"]),
+  ],
+  getCollectors
 );
 
-
+router.get(
+  "/collectors/:date",
+  [
+    passport.authenticate("jwt", {
+      session: false,
+    }),
+    checkPermission(["admin"]),
+  ],
+  getCollectorsByDate
+);
 
 module.exports = router;
